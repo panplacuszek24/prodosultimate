dd49bc1f18fb4cbe89c591aaf99356af4bc5fa7a
Aleksander <79193883+tonyshelly10@users.noreply.github.com>
2022-01-15 17:03:59 +0100

    Add files via upload

---
+++ ROOT/APPS/TESTPROGRAM.bat
@@ -0,0 +1,11 @@
+@echo off
+cls
+title TESTPROGRAM.BAT
+echo.Placek (C) - 2020-2021
+echo.=======================
+echo      test program
+echo.=======================
+echo.
+echo.This is a test program, you're back at the prompt mode.
+
+cmd.exe
